b795b8873520c4489d6e4de843173d880b9af799
Jason Rey <jasonrey@outlook.com>
2016-07-01 20:22:41 +0800

    Updated electron app codes

---
+++ main.js
@@ -0,0 +1,84 @@
+const pug = require('pug');
+const sass = require('node-sass');
+const postcss = require('postcss');
+const autoprefixer = require('autoprefixer');
+const fs = require('fs');
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+let mainWindow;
+
+let createWindow = function() {
+	let processes = [];
+
+	let target = `${__dirname}/web/index.html`;
+
+	processes.push(new Promise((resolve, reject) => {
+		let html = pug.renderFile('web/templates/app.pug', {
+			pretty: true
+		});
+
+		fs.writeFile(target, html, (err) => {
+			if (err) {
+				return reject();
+			}
+
+			resolve();
+		});
+	}));
+
+	processes.push(new Promise((resolve, reject) => {
+		fs.mkdir(__dirname + '/web/assets/css', () => {
+			fs.readdir(__dirname + '/web/assets/sass', (err, items) => {
+				sass.render({
+					file: __dirname + '/web/assets/sass/index.sass',
+					includePaths: [__dirname + '/web/assets/sass']
+				}, (err, result) => {
+					if (err) {
+						console.log(err);
+						reject();
+					} else {
+						postcss([autoprefixer])
+							.process(result.css)
+							.then((result) => {
+								fs.writeFile(__dirname + '/web/assets/css/index.css', result.css, () => {
+									resolve();
+								});
+							});
+					}
+				});
+			});
+		});
+	}));
+
+	Promise.all(processes).then(() => {
+		mainWindow = new BrowserWindow({
+			width: 1000,
+			height: 800,
+			title: 'Database Browser'
+		});
+
+		mainWindow.loadURL(`file://${target}`);
+
+		mainWindow.webContents.openDevTools();
+
+		mainWindow.on('closed', () => {
+			mainWindow = null;
+		});
+	});
+};
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
+});
+
+app.on('activate', function () {
+	if (mainWindow === null) {
+		createWindow();
+	}
+});
