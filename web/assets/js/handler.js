c6a428115209eb61f7115cbd95928b3b9150b11e
Jason Rey <jasonrey@outlook.com>
2016-07-02 00:41:05 +0800

    Updated web handler

--- web/assets/js/handler.js
+++ web/assets/js/handler.js
@@ -15,10 +15,14 @@
 			}
 
 			this.data = JSON.parse(localStorage.dbbr);
+
+			return this;
 		},
 
 		update: function() {
 			localStorage.dbbr = JSON.stringify(this.data);
+
+			return this;
 		},
 
 		get: function(name) {
@@ -42,10 +46,7 @@
 
 			var data = this.data;
 
-			console.log(this.data);
-
 			for (var i = 0; i < segments.length; i++) {
-				console.log(data);
 				if (!data[segments[i]] && typeof data !== 'object') {
 					data = {};
 				}
@@ -61,13 +62,28 @@
 				data = data[segments[i]];
 			}
 
-			console.log(this.data);
-
-			this.update();
+			return this.update();
 		},
 
-		delete: function() {
+		delete: function(name) {
+			var segments = name.split('.');
 
+			var data = this.data;
+
+			for (var i = 0; i < segments.length; i++) {
+				data = data[segments[i]];
+
+				if (i === segments.length - 2) {
+					delete data[segments[i + 1]];
+					break;
+				}
+			}
+
+			return this.update();
+		},
+
+		toString: function() {
+			return JSON.parse(this.data);
 		}
 	};
 
