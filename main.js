f226f2f344f4dc0b4594b903798ae1cedbecae3c
Jason Rey <jasonrey@outlook.com>
2016-07-11 15:01:52 +0800

    Updated files for environment

--- main.js
+++ main.js
@@ -25,34 +25,36 @@ let createWindow = function() {
 
 	let target = `${__dirname}/web/index.html`;
 
-	processes.push(new Promise((resolve, reject) => {
-		let html = pug.renderFile('web/templates/app.pug', {
-			pretty: true
-		});
+	if (environment === 'development') {
+		processes.push(new Promise((resolve, reject) => {
+			let html = pug.renderFile('web/templates/app.pug', {
+				pretty: true
+			});
 
-		fs.writeFile(target, html, (err) => {
-			if (err) {
-				return reject();
-			}
+			fs.writeFile(target, html, (err) => {
+				if (err) {
+					return reject();
+				}
 
-			resolve();
-		});
-	}));
+				resolve();
+			});
+		}));
 
-	processes.push(new Promise((resolve, reject) => {
-		fs.mkdir(__dirname + '/web/assets/css', () => {
-			fs.readdir(__dirname + '/web/assets/sass', (err, items) => {
-				sass.render({
-					file: __dirname + '/web/assets/sass/index.sass',
-					includePaths: [__dirname + '/web/assets/sass']
-				}, (err, result) => {
-					fs.writeFile(__dirname + '/web/assets/css/index.css', result.css, () => {
-						resolve();
+		processes.push(new Promise((resolve, reject) => {
+			fs.mkdir(__dirname + '/web/assets/css', () => {
+				fs.readdir(__dirname + '/web/assets/sass', (err, items) => {
+					sass.render({
+						file: __dirname + '/web/assets/sass/index.sass',
+						includePaths: [__dirname + '/web/assets/sass']
+					}, (err, result) => {
+						fs.writeFile(__dirname + '/web/assets/css/index.css', result.css, () => {
+							resolve();
+						});
 					});
 				});
 			});
-		});
-	}));
+		}));
+	}
 
 	Promise.all(processes).then(() => {
 		Menu.setApplicationMenu(appMenu);
