4edfd6d91e6c550beb9304bebedf79f99fe2cc87
Jason Rey <jasonrey@outlook.com>
2017-07-17 15:37:14 +0800

    Refactor new connection form

--- resources/components/app.vue
+++ resources/components/app.vue
@@ -27,162 +27,7 @@
         :class="{ active: connection === selectedConnection }"
       )
 
-      .active-only.active-flex.abs.abs-full-size(:class="{ active: selectedConnection === null }")
-        .sidebar.flex-no-shrink.overflow-auto
-          serveritem(
-            :server="null"
-            :class="{ active: selectedServer === null }"
-          )
-          serveritem(
-            v-for="server in servers"
-            :key="server.id"
-            :server="server"
-            :class="{ active: selectedServer === server }"
-          )
-
-        .connection-form.flex-grow.flex.overflow-auto
-          .col-xs-6.col-xs-offset-3.text-center(v-show="isConnecting")
-            p Connecting...
-            .btn.btn-danger(@click="isConnecting = false") Cancel
-          form.col-xs-6.col-xs-offset-3(@submit.prevent="createConnection", v-show="!isConnecting")
-            .alert.alert-warning(v-if="connectionError") {{ connectionError }}
-            .form-group
-              input.form-control(placeholder="Connection Name", v-model="newconnection.name", tabindex="1")
-
-            .form-group
-              .connection-colors
-                button.btn.btn-link.btn.xs(type="button", @click="newconnection.color = null")
-                  i.glyphicon.glyphicon-remove
-                button.red(
-                  type="button"
-                  @click="newconnection.color = 'red'"
-                  :class="{ active: newconnection.color === 'red' }"
-                ) Red
-                button.orange(
-                  type="button"
-                  @click="newconnection.color = 'orange'"
-                  :class="{ active: newconnection.color === 'orange' }"
-                ) Orange
-                button.yellow(
-                  type="button"
-                  @click="newconnection.color = 'yellow'"
-                  :class="{ active: newconnection.color === 'yellow' }"
-                ) Yellow
-                button.green(
-                  type="button"
-                  @click="newconnection.color = 'green'"
-                  :class="{ active: newconnection.color === 'green' }"
-                ) Green
-                button.blue(
-                  type="button"
-                  @click="newconnection.color = 'blue'"
-                  :class="{ active: newconnection.color === 'blue' }"
-                ) Blue
-                button.purple(
-                  type="button"
-                  @click="newconnection.color = 'purple'"
-                  :class="{ active: newconnection.color === 'purple' }"
-                ) Purple
-
-            hr
-
-            .form-group
-              input.form-control(
-                tabindex="2"
-                placeholder="Host"
-                v-model="newconnection.host"
-              )
-              input.form-control(
-                tabindex="3"
-                placeholder="Username"
-                v-model="newconnection.username"
-              )
-              input.form-control(
-                tabindex="4"
-                type="password"
-                placeholder="Password"
-                v-model="newconnection.password"
-              )
-              input.form-control(
-                tabindex="5"
-                placeholder="Database"
-                v-model="newconnection.database"
-              )
-              input.form-control(
-                tabindex="6"
-                type="number"
-                placeholder="Port"
-                v-model="newconnection.port"
-              )
-
-            .checkbox
-              label
-                input(
-                  tabindex="7"
-                  type="checkbox"
-                  v-model="newconnection.useSSH"
-                )
-                =" Use SSH"
-
-            .form-group(v-show="newconnection.useSSH")
-              input.form-control(
-                tabindex="8"
-                placeholder="SSH Host"
-                v-model="newconnection.sshhost"
-              )
-              input.form-control(
-                tabindex="9"
-                placeholder="SSH Username"
-                v-model="newconnection.sshusername"
-              )
-              input.form-control(
-                tabindex="10"
-                type="password"
-                placeholder="SSH Password"
-                v-model="newconnection.sshpassword"
-              )
-              input.form-control(
-                tabindex="11"
-                type="number"
-                placeholder="SSH Port"
-                v-model="newconnection.sshport"
-              )
-
-            hr
-
-            .btn-group.btn-group-justified
-              .btn-group
-                button.btn.btn-block.btn-lg.btn-danger(
-                  type="button"
-                  tabindex="14"
-                  @click="selectServer(null)"
-                )
-                  i.glyphicon.glyphicon-remove
-
-              .btn-group(v-if="selectedServer")
-                button.btn.btn-block.btn-lg.btn-primary(
-                  type="button"
-                  tabindex="13"
-                  :disabled="!formFilled"
-                  @click="saveServer(newconnection)"
-                )
-                  i.glyphicon.glyphicon-floppy-saved
-
-              .btn-group(v-if="!selectedServer")
-                button.btn.btn-block.btn-lg.btn-primary(
-                  type="button"
-                  tabindex="13"
-                  :disabled="!formFilled"
-                  @click="saveServer(newconnection)"
-                )
-                  i.glyphicon.glyphicon-plus
-
-              .btn-group
-                button.btn.btn-block.btn-lg.btn-success(
-                  tabindex="12"
-                  :disabled="!formFilled"
-                )
-                  i.glyphicon.glyphicon-ok
+      newconnection(:class="{ active: selectedConnection === null }")
 
       .active-only.abs.abs-full-size.overflow-auto(:class="{ active: selectedConnection === false }")
         logitem(
@@ -271,10 +116,12 @@
 </style>
 
 <script>
-  import { mapState, mapMutations, mapActions } from 'vuex'
+  import { mapState, mapMutations } from 'vuex'
+
+
   import servernav from './servernav.vue'
   import servercontent from './servercontent.vue'
-  import serveritem from './serveritem.vue'
+  import newconnection from './newconnection.vue'
 
   import logitem from './logitem.vue'
 
@@ -285,7 +132,7 @@
     components: {
       servernav,
       servercontent,
-      serveritem,
+      newconnection,
       logitem
     },
 
@@ -309,25 +156,6 @@
         }
 
         return this.$store.state.log.items.slice().reverse()
-      },
-
-      formFilled() {
-        if (!this.newconnection.host ||
-          !this.newconnection.username ||
-          !this.newconnection.password ||
-          !this.newconnection.port || (
-            this.newconnection.useSSH && (
-              !this.newconnection.sshhost ||
-              !this.newconnection.sshusername ||
-              !this.newconnection.sshpassword ||
-              !this.newconnection.sshport
-            )
-          )
-        ) {
-          return false
-        }
-
-        return true
       }
     },
 
@@ -338,15 +166,6 @@
     methods: {
       ...mapMutations('connection', {
         selectConnection: 'select'
-      }),
-
-      ...mapActions('connection', {
-        createConnection: 'create'
-      }),
-
-      ...mapActions('server', {
-        selectServer: 'select',
-        saveServer: 'save'
       })
     }
   }
---
+++ resources/components/newconnection.vue
@@ -0,0 +1,215 @@
+<template lang="pug">
+  .active-only.active-flex.abs.abs-full-size
+    .sidebar.flex-no-shrink.overflow-auto
+      serveritem(
+        :server="null"
+        :class="{ active: selectedServer === null }"
+      )
+      serveritem(
+        v-for="server in servers"
+        :key="server.id"
+        :server="server"
+        :class="{ active: selectedServer === server }"
+      )
+
+    .connection-form.flex-grow.flex.overflow-auto
+      .col-xs-6.col-xs-offset-3.text-center(v-show="isConnecting")
+        p Connecting...
+        .btn.btn-danger(@click="isConnecting = false") Cancel
+      form.col-xs-6.col-xs-offset-3(@submit.prevent="createConnection", v-show="!isConnecting")
+        .alert.alert-warning(v-if="connectionError") {{ connectionError }}
+        .form-group
+          input.form-control(placeholder="Connection Name", v-model="newconnection.name", tabindex="1")
+
+        .form-group
+          .connection-colors
+            button.btn.btn-link.btn.xs(type="button", @click="newconnection.color = null")
+              i.glyphicon.glyphicon-remove
+            button.red(
+              type="button"
+              @click="newconnection.color = 'red'"
+              :class="{ active: newconnection.color === 'red' }"
+            ) Red
+            button.orange(
+              type="button"
+              @click="newconnection.color = 'orange'"
+              :class="{ active: newconnection.color === 'orange' }"
+            ) Orange
+            button.yellow(
+              type="button"
+              @click="newconnection.color = 'yellow'"
+              :class="{ active: newconnection.color === 'yellow' }"
+            ) Yellow
+            button.green(
+              type="button"
+              @click="newconnection.color = 'green'"
+              :class="{ active: newconnection.color === 'green' }"
+            ) Green
+            button.blue(
+              type="button"
+              @click="newconnection.color = 'blue'"
+              :class="{ active: newconnection.color === 'blue' }"
+            ) Blue
+            button.purple(
+              type="button"
+              @click="newconnection.color = 'purple'"
+              :class="{ active: newconnection.color === 'purple' }"
+            ) Purple
+
+        hr
+
+        .form-group
+          input.form-control(
+            tabindex="2"
+            placeholder="Host"
+            v-model="newconnection.host"
+          )
+          input.form-control(
+            tabindex="3"
+            placeholder="Username"
+            v-model="newconnection.username"
+          )
+          input.form-control(
+            tabindex="4"
+            type="password"
+            placeholder="Password"
+            v-model="newconnection.password"
+          )
+          input.form-control(
+            tabindex="5"
+            placeholder="Database"
+            v-model="newconnection.database"
+          )
+          input.form-control(
+            tabindex="6"
+            type="number"
+            placeholder="Port"
+            v-model="newconnection.port"
+          )
+
+        .checkbox
+          label
+            input(
+              tabindex="7"
+              type="checkbox"
+              v-model="newconnection.useSSH"
+            )
+            =" Use SSH"
+
+        .form-group(v-show="newconnection.useSSH")
+          input.form-control(
+            tabindex="8"
+            placeholder="SSH Host"
+            v-model="newconnection.sshhost"
+          )
+          input.form-control(
+            tabindex="9"
+            placeholder="SSH Username"
+            v-model="newconnection.sshusername"
+          )
+          input.form-control(
+            tabindex="10"
+            type="password"
+            placeholder="SSH Password"
+            v-model="newconnection.sshpassword"
+          )
+          input.form-control(
+            tabindex="11"
+            type="number"
+            placeholder="SSH Port"
+            v-model="newconnection.sshport"
+          )
+
+        hr
+
+        .btn-group.btn-group-justified
+          .btn-group
+            button.btn.btn-block.btn-lg.btn-danger(
+              type="button"
+              tabindex="14"
+              @click="selectServer(null)"
+            )
+              i.glyphicon.glyphicon-remove
+
+          .btn-group(v-if="selectedServer")
+            button.btn.btn-block.btn-lg.btn-primary(
+              type="button"
+              tabindex="13"
+              :disabled="!formFilled"
+              @click="saveServer(newconnection)"
+            )
+              i.glyphicon.glyphicon-floppy-saved
+
+          .btn-group(v-if="!selectedServer")
+            button.btn.btn-block.btn-lg.btn-primary(
+              type="button"
+              tabindex="13"
+              :disabled="!formFilled"
+              @click="saveServer(newconnection)"
+            )
+              i.glyphicon.glyphicon-plus
+
+          .btn-group
+            button.btn.btn-block.btn-lg.btn-success(
+              tabindex="12"
+              :disabled="!formFilled"
+            )
+              i.glyphicon.glyphicon-ok
+</template>
+
+<script>
+  import { mapState, mapActions } from 'vuex'
+
+  import serveritem from './serveritem.vue'
+
+  export default {
+    components: {
+      serveritem
+    },
+
+    computed: {
+      ...mapState('server', {
+        servers: 'items',
+        selectedServer: 'selected',
+      }),
+
+      ...mapState('connection', {
+        connections: 'items',
+        isConnecting: 'connecting',
+        connectionError: 'error',
+        newconnection: 'form'
+      }),
+
+      formFilled() {
+        if (!this.newconnection.host ||
+          !this.newconnection.username ||
+          !this.newconnection.password ||
+          !this.newconnection.port || (
+            this.newconnection.useSSH && (
+              !this.newconnection.sshhost ||
+              !this.newconnection.sshusername ||
+              !this.newconnection.sshpassword ||
+              !this.newconnection.sshport
+            )
+          )
+        ) {
+          return false
+        }
+
+        return true
+      }
+    },
+
+    methods: {
+      ...mapActions('connection', {
+        createConnection: 'create'
+      }),
+
+      ...mapActions('server', {
+        selectServer: 'select',
+        saveServer: 'save'
+      })
+    }
+  }
+
+</script>
